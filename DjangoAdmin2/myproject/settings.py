b3fb866e98af96b6d1f51b72695df36fc214a50b
ChungCheng <lf2net67983@gmail.com>
2025-02-23 18:35:31 +0800

    定版V0.1

--- DjangoAdmin2/myproject/settings.py
+++ DjangoAdmin2/myproject/settings.py
@@ -143,7 +143,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'theme_entertainment', 'static'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
